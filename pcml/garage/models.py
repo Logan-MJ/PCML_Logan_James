6ba80642cffb9978f33c3655926cadd5df1c4b9a
Logan J <loganmj.2015@gmail.com>
2025-10-06 14:37:04 -0400

    Add models and admin site

--- pcml/garage/models.py
+++ pcml/garage/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return str(self.year) + " " + self.make + " " + self.model
+
+class MaintenanceEvent(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    odometer = models.IntegerField()
+    event_date = models.DateField()
+    description = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return str(self.event_date) + " " + self.description
